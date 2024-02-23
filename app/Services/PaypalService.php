b9ac15b11277618b032ea1d1238acb0d583f741d
moh-shawwa <malshawwa@trustangle.com>
2024-02-23 18:15:05 +0200

    create and retrieve order api in paypal

--- app/Services/PaypalService.php
+++ app/Services/PaypalService.php
@@ -137,7 +137,7 @@ class PaypalService
     public function captureOrder($orderId)
     {
         $this->token = $this->token ?? $this->generateAccessToken();
-dd($this->token);
+
         $curl = curl_init();
         curl_setopt_array($curl, array(
             CURLOPT_URL => $this->baseUrl . '/v2/checkout/orders/' . $orderId . '/capture',
@@ -149,8 +149,7 @@ dd($this->token);
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => 'POST',
             CURLOPT_HTTPHEADER => array(
-                'Authorization: Bearer ' . $this->token,
-                'Content-Type: application/json'
+                'Authorization: Bearer ' . $this->token
             ),
         ));
 
