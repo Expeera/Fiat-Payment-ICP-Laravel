6d5b3ea3c54d2db6e8bc7593bc1f90704936421d
moh-shawwa <malshawwa@trustangle.com>
2024-03-07 12:36:10 +0200

    create and retrieve order api in paypal

--- app/Http/Controllers/Api/StripeController.php
+++ app/Http/Controllers/Api/StripeController.php
@@ -27,6 +27,11 @@ class StripeController extends Controller
     {
         try {
 
+            return responseJson(true, "Success", [
+                'id' => "sadasdasdasdasdasd",
+                'url' => "http:sdsadsadasdasd",
+            ], 200);
+
             Log::info("Get Secret key");
             $stripe = new StripeService($request->get("secret_key"));
             Log::info("Start to create session");
