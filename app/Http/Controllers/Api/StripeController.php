7e587f90c6898a2da043bc3780f880a2aea1efea
moh-shawwa <malshawwa@trustangle.com>
2024-03-07 12:46:03 +0200

    create and retrieve order api in paypal

--- app/Http/Controllers/Api/StripeController.php
+++ app/Http/Controllers/Api/StripeController.php
@@ -27,51 +27,11 @@ class StripeController extends Controller
     {
         try {
 
-
-
             Log::info("Get Secret key");
             $stripe = new StripeService($request->get("secret_key"));
             Log::info("Start to create session");
 
-
-$data = $request->toArray();
-//            $res = $stripe->createSession($request->toArray());
-
-            $formData = [
-                'payment_method_types[0]' => 'card',
-                'line_items[0][price_data][currency]' => $data['currency'],
-                'line_items[0][price_data][unit_amount]' => $data['unit_amount'],
-                'line_items[0][quantity]' => $data['quantity'],
-                'line_items[0][price_data][product_data][name]' => 'Token',
-                'mode' => 'payment',
-                'success_url' => $data['success_url'],
-                'cancel_url' => $data['cancel_url']
-            ];
-
-            $postData = http_build_query($formData);
-
-            $curl = curl_init();
-            curl_setopt_array($curl, array(
-                CURLOPT_URL => 'https://api.stripe.com/v1/checkout/sessions',
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => '',
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 0,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => 'POST',
-                CURLOPT_POSTFIELDS => $postData,
-                CURLOPT_HTTPHEADER => array(
-                    'Authorization: Bearer ' . $request->get("secret_key"),
-                    'Content-Type: application/x-www-form-urlencoded'
-                ),
-            ));
-
-            $res = curl_exec($curl);
-            $status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
-            curl_close($curl);
-
+            $res = $stripe->createSession($request->toArray());
             Log::info("End to create session");
 
             Log::info("Check if there an error");
