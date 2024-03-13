c9bae2911cc02c89a4c4b8016296429ff38f4f20
moh-shawwa <malshawwa@trustangle.com>
2024-03-13 12:45:13 +0200

    create and retrieve order api in paypal

--- app/Http/Controllers/Api/StripeController.php
+++ app/Http/Controllers/Api/StripeController.php
@@ -39,34 +39,26 @@ class StripeController extends Controller
 
             $invoiceNumber = $request->headers->get("invoice-number");
             Log::info($invoiceNumber);
-            $invoiceNumberFromCache = \Illuminate\Support\Facades\Cache::get($invoiceNumber, json_encode([
-                "id" => "",
-                "url" => ""
-            ]));
+            $invoiceNumberFromCache = \Illuminate\Support\Facades\Cache::get($invoiceNumber, "");
 
-            $resCache = json_decode($invoiceNumberFromCache, true);
-            if (empty($resCache["id"])) {
-                \Illuminate\Support\Facades\Cache::set($invoiceNumber, [
-                    "id" => $res['id'],
-                    "url" => $res['url'],
-                ]);
+//            $resCache = json_decode($invoiceNumberFromCache, true);
+            if (empty($invoiceNumberFromCache)) {
+                \Illuminate\Support\Facades\Cache::set($invoiceNumber, $res['id'] . ":-:" . $res["url"]);
             }
 
-            $invoiceNumberFromCache = \Illuminate\Support\Facades\Cache::get($invoiceNumber, json_encode([
-                "id" => "",
-                "url" => ""
-            ]));
-            $resCache = json_decode($invoiceNumberFromCache , true);
+            $invoiceNumberFromCache = \Illuminate\Support\Facades\Cache::get($invoiceNumber, "");
+//            $resCache = json_decode($invoiceNumberFromCache , true);
 
-            Log::info($invoiceNumber . " => return " . $resCache['id']);
+            Log::info($invoiceNumber . " => return " . $invoiceNumberFromCache);
 //$re = Random::generate(10);
 //            Log::info("6- retuern success " . $re);
 
 //        return responseJson(true, "Success", Random::generate(10), 200);
 
+            $r = explode($invoiceNumber, ":-:");
             return responseJson(true, "Success", json_encode([
-                'id' => $resCache['id'],
-                'url' => $resCache['url'],
+                'id' => $r[0],
+                'url' => $r[1],
             ]), 200);
 
         } catch (\Exception $e) {
